61e00a008a24cef8ca1de84b64f7c66992e23b79
Pedro Felix <pedro123felixcavalcanti@gmail.com>
2024-11-04 17:53:21 -0300

    task six

--- src/Entities/Assessment.java
+++ src/Entities/Assessment.java
@@ -2,23 +2,23 @@ package Entities;
 
 public class Assessment{
 
-    private int idAssessment;
+    private Long idAssessment;
     private int grade; //nota
     private Order order;
     private Restaurant restaurant;
 
-    public Assessment(int idAssessment, int grade, Order order, Restaurant restaurant) {
+    public Assessment(Long idAssessment, int grade, Order order, Restaurant restaurant) {
         this.idAssessment = idAssessment;
         this.grade = grade;
         this.order = order;
         this.restaurant = restaurant;
     }
 
-    public int getIdAssessment() {
+    public Long getIdAssessment() {
         return idAssessment;
     }
 
-    public void setIdAssessment(int idAssessment) {
+    public void setIdAssessment(Long idAssessment) {
         this.idAssessment = idAssessment;
     }
 
